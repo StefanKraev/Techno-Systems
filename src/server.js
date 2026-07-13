63f0371ab7cb305a3ca221db712293a0fd97fc5c
StefanKraev <stefanemilovkraev@abv.bg>
2026-07-13 16:00:34 +0300

    Created initial server

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, '../public', 'index.html');
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('File did not load.');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
